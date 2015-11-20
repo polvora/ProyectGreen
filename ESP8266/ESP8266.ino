9cefe724a40354885a55666262fcd43ffcd09003
Marcelo Tapia <t.marcelo.p@gmail.com>
2015-11-20 03:06:46 -0300

    Added HTML template for interface, status tab already designed, needs javascript and arduino code to update data.

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
+#include <FS.h>
 #include <Ticker.h>
 
 #define DEBUG // Comentado default
@@ -44,8 +45,21 @@ void setup() {
   dnsServer.start(DNS_PORT, "*", apIP);
   dnsServer.setTTL(300);
 
+  // Configuracion Sistema de Arcvhivos SPIFFS
+  SPIFFS.begin();
+
   // Configuracion Servidor Web
-  webServer.on("/", handleRoot);
+  webServer.onNotFound([](){
+    String path = webServer.uri();
+    if(path.endsWith("/")) path += "index.htm";
+    String contentType = getContentType(path);
+    if(SPIFFS.exists(path)) {
+      File file = SPIFFS.open(path, "r");
+      webServer.streamFile(file, contentType);
+      file.close();
+    }
+    else webServer.send(404, "text/html", "<strong>404</strong> File Not Found");
+  });
   webServer.begin();
 
   // Configuracion Estacion WiFi
@@ -56,19 +70,22 @@ void setup() {
 }
 
 void loop() {
-  delay(200);
   dnsServer.processNextRequest();
 	webServer.handleClient();
 }
 
-void handleRoot() {
-  char temp[400];
-  snprintf(temp, sizeof temp, "<h1>You are Connected</h1><br><p>%s</p>", staConnected?"yes":"no");
-  webServer.send(200, "text/html", temp);
+String getContentType(String filename){
+  if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
 }
-
-
-
-
-
-
